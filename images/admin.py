a436f2c2c5c623c33374e52629d120f1afc70d9c
rwyckoff <Shpob@hotmail.com>
2019-12-17 22:51:10 -0700

    Deleted the old images app

--- images/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import UploadedImage
-
-
-@admin.register(UploadedImage)
-class UserAdmin(admin.ModelAdmin):
-    pass
--- images/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from django.urls import path
-
-from images import views
-
-
-urlpatterns = [
-    path('image/upload/', views.UploadedImagesView.as_view(), name='image-upload'),
-]
