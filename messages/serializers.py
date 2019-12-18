a23def9f78b11feed94dd8924b9d2b7a58055531
rwyckoff <Shpob@hotmail.com>
2019-12-17 19:26:05 -0700

    Changed Django admin page title.

--- messages/serializers.py
+++ messages/serializers.py
@@ -3,10 +3,11 @@ from messages.models import Message
 
 
 class MessageSerializer(serializers.HyperlinkedModelSerializer):
-    sender = serializers.CharField()
-    receiver = serializers.CharField()
+    sender = serializers.CharField(max_length=120)
+    receiver = serializers.CharField(max_length=120)
 
     class Meta:
         model = Message
         fields = ('url', 'id', 'created', 'sender', 'receiver', 'subject', 'content')
         read_only_fields = ('created', 'id')
+
